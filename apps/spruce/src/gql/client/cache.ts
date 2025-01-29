06098a3903a031828d8810b6c4da0baa889f1d40
Sophie Stadler <sophie.stadler@mongodb.com>
2025-01-29 13:23:09 -0500

    Add version caching

--- apps/spruce/src/gql/client/cache.ts
+++ apps/spruce/src/gql/client/cache.ts
@@ -1,5 +1,6 @@
 import { InMemoryCache } from "@apollo/client";
 import { IMAGE_EVENT_LIMIT } from "pages/image/tabs/EventLogTab/useImageEvents";
+import { WaterfallQuery } from "../generated/types";
 
 export const cache = new InMemoryCache({
   typePolicies: {
@@ -16,6 +17,136 @@ export const cache = new InMemoryCache({
         },
         hasVersion: {
           keyArgs: ["$patchId"],
+        },
+        waterfall: {
+          keyArgs: ["options", ["projectIdentifier"]],
+          read(existing, { args, readField, variables }) {
+            if (!existing) {
+              return undefined;
+            }
+
+            const minOrder = args?.options?.minOrder ?? 0;
+            const maxOrder = args?.options?.maxOrder ?? 0;
+            const limit = variables?.options?.limit ?? 0;
+
+            const existingVersions =
+              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
+                "flattenedVersions",
+                existing,
+              ) ?? [];
+
+            const idx = existingVersions.findIndex((v) => {
+              const versionOrder = readField<number>("order", v) ?? 0;
+              if (minOrder) {
+                return versionOrder - 1 === minOrder;
+              }
+              if (maxOrder) {
+                return versionOrder + 1 === maxOrder;
+              }
+              return false;
+            });
+            if (idx === -1) {
+              return undefined;
+            }
+            let startIndex = maxOrder ? idx : 0;
+            let endIndex = maxOrder ? existingVersions.length : idx;
+            let numActivated = 0;
+
+            // Count backwards for paginating backwards.
+            if (minOrder) {
+              for (let i = endIndex; i >= 0; i--) {
+                if (readField<boolean>("activated", existingVersions[i])) {
+                  numActivated += 1;
+                  if (numActivated === limit) {
+                    startIndex = i;
+                    break;
+                  }
+                }
+              }
+            }
+
+            // Count forwards for paginating forwards.
+            if (maxOrder) {
+              for (let i = startIndex; i < existingVersions.length; i++) {
+                if (readField<boolean>("activated", existingVersions[i])) {
+                  numActivated += 1;
+                  if (numActivated === limit) {
+                    endIndex = i;
+                    break;
+                  }
+                }
+              }
+            }
+
+            // need code for leading inactive versions
+
+            // Add 1 because slice is [inclusive, exclusive).
+            const flattenedVersions = existingVersions.slice(
+              startIndex,
+              endIndex + 1,
+            );
+
+            console.log(
+              existingVersions.length,
+              flattenedVersions,
+              startIndex,
+              endIndex,
+            );
+
+            console.log(
+              flattenedVersions[0],
+              flattenedVersions[flattenedVersions.length - 1],
+            );
+            const prevOrderNumber =
+              readField<number>("order", flattenedVersions[0]) ?? 0;
+            const nextOrderNumber =
+              readField<number>(
+                "order",
+                flattenedVersions[flattenedVersions.length - 1],
+              ) ?? 0;
+
+            return {
+              flattenedVersions,
+              pagination: {
+                prevPageOrder: prevOrderNumber,
+                nextPageOrder: nextOrderNumber,
+                hasNextPage: nextOrderNumber > 0,
+                hasPrevPage: prevOrderNumber > 0,
+              },
+            };
+          },
+          merge(existing, incoming, { readField }) {
+            const existingVersions =
+              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
+                "flattenedVersions",
+                existing,
+              ) ?? [];
+            const incomingVersions =
+              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
+                "flattenedVersions",
+                incoming,
+              ) ?? [];
+            const versions = [...existingVersions, ...incomingVersions];
+
+            // Use a map to enforce that there are no duplicates.
+            const versionsMap = new Map();
+            versions.forEach((v) => {
+              const order = readField<number>("order", v) ?? 0;
+              versionsMap.set(order, v);
+            });
+
+            const v = Array.from(versionsMap.values()).sort((a, b) => {
+              const aOrder = readField<number>("order", a) ?? 0;
+              const bOrder = readField<number>("order", b) ?? 0;
+              return bOrder - aOrder;
+            });
+
+            const pagination = readField("pagination", incoming);
+            return {
+              flattenedVersions: v,
+              pagination,
+            };
+          },
         },
       },
     },
